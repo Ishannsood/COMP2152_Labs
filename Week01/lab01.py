bfb7ac9f05d9a4de97b72dadbdc497fd0fd51541
Ishannsood <soodishan03@gmail.com>
2026-01-18 11:21:53 -0500

    lab01 done with answers

--- Week01/lab01.py
+++ Week01/lab01.py
@@ -1,5 +1,25 @@
 # Sample Coding Questions 01 Week 01 
-# Ishan Sood
+# Ishan Sood - 101539944
 
 
 #Question2 
+my_array = [1,2,3,4]
+
+#Question3: Order of Operations
+
+a=1
+b=2
+c=3
+d=4
+
+#full bracketed Version: e = a - b ** c // d + a % c 
+e= (a - ((b ** c // d  ))) + ( a % c )
+
+#Question4 : Formatting 
+temperature = 32.6
+print("the temperature today is: {:.3f} degrees celsius".format(temperature))
+
+#Question5: User Input 
+userAge= int(input("Enter your age here: "))
+userAge = userAge + 22
+print("Now showing the shop items filter by age: " , userAge)
